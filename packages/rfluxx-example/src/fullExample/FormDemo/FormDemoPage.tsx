0edef499405fce92409b9c4c7bcb8f25fc4c8f3a
Jochen Grün <jochen.gruen@googlemail.com>
2019-05-24 21:39:16 +0200

    Pimped up the form demo page.

--- packages/rfluxx-example/src/fullExample/FormDemo/FormDemoPage.tsx
+++ packages/rfluxx-example/src/fullExample/FormDemo/FormDemoPage.tsx
@@ -1,3 +1,5 @@
+import { Snackbar, Typography, Grid } from "@material-ui/core";
+import { createStyles, WithStyles, withStyles } from "@material-ui/styles";
 import * as React from "react";
 import { StoreSubscription } from "rfluxx";
 import { Form, IFormStore, IFormStoreState, StringFormField } from "rfluxx-forms";
@@ -5,44 +7,99 @@ import { IPageContextProps, PageContext } from "rfluxx-routing";
 
 import { IFormData } from "./IFormData";
 
-export interface IFormDemoPageProps extends IPageContextProps
+export const styles = createStyles({
+    root: {
+
+    },
+    formData: {
+        backgroundColor: "lightgray",
+        borderStyle: "dotted",
+        borderWidth: "1px",
+        borderColor: "gray",
+        borderRadius: "5px",
+        padding: "10px",
+        marginTop: "50px"
+    }
+});
+
+export interface IFormDemoPageProps extends IPageContextProps, WithStyles<typeof styles>
 {
 }
 
 export interface IFormDemoPageState {
+    formData: IFormData;
 }
 
-export class FormDemoPage extends React.Component<IFormDemoPageProps, IFormDemoPageState>
-{
-    private formStore: IFormStore<IFormData>;
-
-    constructor(props: any)
+export const FormDemoPage = withStyles(styles)(
+    class extends React.Component<IFormDemoPageProps, IFormDemoPageState>
     {
-        super(props);
+        private subscription: StoreSubscription<IFormStore<IFormData>, IFormStoreState<IFormData>>
+            = new StoreSubscription();
 
-        this.state = {
-            contextInfo: "none given",
-            selectedString: ""
-        };
+        constructor(props: any)
+        {
+            super(props);
 
-        this.formStore = this.props.container.resolve<IFormStore<IFormData>>("IFormStore<IFormData>");
+            this.state = {
+                formData: null
+            };
+        }
+
+        public componentDidMount(): void
+        {
+            const formStore = this.props.container.resolve<IFormStore<IFormData>>("IFormStore<IFormData>");
+            this.subscription.subscribeStore(formStore, s =>
+            {
+                this.setState({ ...this.state, formData: s.data});
+            });
+        }
+
+        public componentWillUnmount(): void
+        {
+            this.subscription.unsubscribe();
+        }
+
+        public render(): any
+        {
+            const { classes, ...rest } = this.props;
+
+            return <div className="container-fluid">
+                <h1>Select a string</h1>
+                <p>
+                    On this page you can select a string that will be handed back to the calling control.
+                </p>
+
+                <Form formStore={this.subscription.store}>
+                    <Grid container direction="column" spacing={8}>
+                        <Grid item>
+                            <StringFormField getValue={(d: IFormData) => d.firstName}
+                                            setValue={(d: IFormData, value: string) => d.firstName = value}
+                                            label="First Name"
+                                            description="Please enter your first name"
+                                            required></StringFormField>
+                        </Grid>
+                        <Grid item>
+                            <StringFormField getValue={(d: IFormData) => d.lastName}
+                                            setValue={(d: IFormData, value: string) => d.lastName = value}
+                                            label="Last Name"
+                                            description="Please enter your last name"
+                                            required></StringFormField>
+                        </Grid>
+                    </Grid>
+                </Form>
+
+                <div className={classes.formData}>
+                    <Typography>
+                        Here is the data you entered:
+                    </Typography>
+                    <Typography>
+                        { this.state.formData &&
+                            <pre>
+                                {JSON.stringify(this.state.formData, null, 2)}
+                            </pre> }
+                    </Typography>
+                </div>
+            </div>;
+        }
     }
-
-    public render(): any
-    {
-        return <div className="container-fluid">
-            <h1>Select a string</h1>
-            <p>
-                On this page you can select a string that will be handed back to the calling control.
-            </p>
-
-            <Form formStore={this.formStore}>
-                <StringFormField getValue={(d: IFormData) => d.firstName}
-                                 setValue={(d: IFormData, value: string) => d.firstName = value}
-                                 label="First Name"
-                                 description="Please enter your first name"
-                                 required></StringFormField>
-            </Form>
-        </div>;
-    }
-}
+);
